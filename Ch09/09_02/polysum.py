89bb02aaa79a018f4767d9a2a2dc6b1a70a3fe48
Miki Tebeka <miki@353solutions.com>
2022-08-22 12:21:21 +0300

    tabs -> spaces

--- Ch09/09_02/polysum.py
+++ Ch09/09_02/polysum.py
@@ -1,9 +1,9 @@
 # %%
 def polyn(n):
-	total = 0
-	for i in range(n):
-	    total += (7*n*n) + (-3*n) + 42
-	return total
+    total = 0
+    for _ in range(n):
+        total += (7*n*n) + (-3*n) + 42
+    return total
 
 %timeit -n 10_000 polyn(1000)
 
@@ -12,9 +12,9 @@ import numba
 
 @numba.jit
 def polyn_jit(n):
-	total = 0
-	for i in range(n):
-	    total += (7*n*n) + (-3*n) + 42
-	return total
+    total = 0
+    for _ in range(n):
+        total += (7*n*n) + (-3*n) + 42
+    return total
 
 %timeit -n 10_000 polyn_jit(1000)
